20ec2b64cef96e8b4d790488c053f9cc305270a5
Jayson Thompson <jaysonthompson922@gmail.com>
2023-06-13 08:11:01 -0700

    formats comments and adds active state to navbar links

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 import RouterHandler from './router.js'
 
+window.onhashchange = () => {
+  setActiveLink()
+}
+
+const setActiveLink = () => {
+  const links = document.querySelectorAll('.header-link')
+  links.forEach(link => {
+    const linkPath = link.getAttribute('href')
+    const currentPath = window.location.hash
+    if(linkPath === currentPath) {
+      link.classList.add('active')
+    } else {
+      link.classList.remove('active')
+    }
+  })
+}
+
 class App {
   constructor() {
     new RouterHandler()
   }  
 }
+
+
 
 new App();
